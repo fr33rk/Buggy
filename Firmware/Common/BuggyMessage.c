56a765fae796b7042b79ee210d7a25ab671c9247
fr33rk <github@deleeuw.frl>
2016-12-19 12:49:25 +0100

    Forgot to commit the new files.

---
+++ Firmware/Common/BuggyMessage.c
@@ -0,0 +1,13 @@
+#include "BuggyMemory.h"
+#include "BuggyMessage.h"
+
+void CreateVersionMessage(BuggyMessage message)
+{
+    /*
+    message.Command = cmdVersion;
+    message.DataSize = 3;
+    
+    message.Data[0] = '0';
+    message.Data[1] = '1';
+     */
+}
