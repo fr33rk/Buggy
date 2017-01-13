49c551bfefb4e7ffce94b6c2bfa3ca46c5c38553
Freerk de Leeuw <freerk@deleeuw.frl>
2017-01-13 16:54:06 +0100

    Added empty service.

---
+++ Software/BuggySoft/PL.BuggySoft.Business/Services/BuggyCommunicationService.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PL.Common.Socket;
+
+namespace PL.BuggySoft.Business.Services
+{
+	public class BuggyCommunicationService
+	{
+		private ISender mSender;
+
+		public BuggyCommunicationService(ISender sender)
+		{
+			mSender = sender;
+			mSender.OnConnect += SenderOnOnConnect; 
+			mSender.OnDataReceived += SenderOnOnDataReceived;
+			mSender.OnDisconnect += SenderOnOnDisconnect;
+		}
+
+		private void SenderOnOnDisconnect(object sender, EventArgs eventArgs)
+		{
+			throw new NotImplementedException();
+		}
+
+		private void SenderOnOnConnect(object sender, EventArgs eventArgs)
+		{
+			throw new NotImplementedException();
+		}
+
+		private void SenderOnOnDataReceived(object sender, SocketDataReceivedEventArgs socketDataReceivedEventArgs)
+		{
+			throw new NotImplementedException();
+		}
+
+
+
+		public void Connect()
+		{
+			mSender?.Start();
+		}
+	}
+}
