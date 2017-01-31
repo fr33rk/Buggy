6494e964c49684ba6b1a718b0fcd261df93781b8
Freerk de Leeuw <freerk@deleeuw.frl>
2017-01-31 16:43:41 +0100

    Split the sensor result messages.

---
+++ Software/BuggySoft/BuggySoft.TestTool/ViewModels/SensorReadingsVM.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PL.BuggySoft.Infrastructure.Models.Messages;
+
+namespace BuggySoft.TestTool.ViewModels
+{
+	public class SensorReadingsVM
+	{
+		public void Update(SensorResultMessageWrapper sensorMessage)
+		{
+			
+		}
+
+		public void Update(SensorResultAllMessageWrapper sensorMessage)
+		{
+			
+		}		
+	}
+}
